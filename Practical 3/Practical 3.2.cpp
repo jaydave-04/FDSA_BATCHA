86b3e66d96303940de7b38801f0b90428100c4c5
jaydave-04 <jayjdaveofficial02@gmail.com>
2026-08-17 15:52:09 +0530

    Add files via upload

---
+++ Practical 3/Practical 3.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[50];
+    int i,j,temp,n;
+
+    cout<<"Enter the number of colours you want to enter:- "<<endl;
+    cin>>n;
+
+    for(i=0;i<n;i++){
+        cout<<"Enter Colour code of bucket number "<<i+1<<":- ";
+        cin>>arr[i];
+    }
+
+    for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+
+            temp=arr[i];
+            if(arr[i]>arr[j]){
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+
+
+            }
+}
+
+    cout<<"Sorted array is"<<endl;
+    for(i=0;i<n;i++){
+        cout<<arr[i];
+
+
+}
+}
